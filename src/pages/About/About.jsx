c2b1d33487b61dc8df14b14ce183cbcd2a08c229
Mateus Santos <50279606+Matag-e@users.noreply.github.com>
2024-07-22 15:55:59 -0300

    Update About.jsx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -10,8 +10,7 @@ const About = () => {
         Sobre o Mini <span>Blog</span>
       </h2>
       <p>
-        Este projeto consiste em um blog feito com React no front-end e Firebase
-        no back-end
+        O miniblog desenvolvido combina a simplicidade do Instagram com funcionalidades que incentivam a interação social. Utilizando React no front-end e Firebase no back-end, os usuários podem compartilhar fotos, interagir através de curtidas e comentários, além de descobrir novos conteúdos por meio de um feed personalizado. É uma plataforma intuitiva e dinâmica para conectar pessoas através de imagens inspiradoras e conversas significativas.
       </p>
       <Link to="/posts/create" className="btn">
         Criar Post
